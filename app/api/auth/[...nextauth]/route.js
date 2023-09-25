7ce2f1dcaffa29f3d1f51acfcb09d7af746a5850
Hiftie <127197446+hiftielabs@users.noreply.github.com>
2023-09-25 22:25:18 +0530

    Google oAuth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,5 @@
+import Credentials from "@/app/schemas/user";
+import { Connection } from "@/app/utils/mongodb";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -9,25 +11,30 @@ const authOptions = {
         }),
     ],
     callbacks: {
-        async signIn({ user, account}) {
-            
-            if (account.provider === 'google' ) {
+        async signIn({ user, account }) {
+
+            if (account.provider === 'google') {
 
                 const { email, name } = user;
 
                 try {
-                   const res =  await fetch("https://collections-lemon.vercel.app/api/user", {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                        body: JSON.stringify({
-                            name, email
-                        })
-                    });
+                    await Connection();
+                    const ExistingCredentials = await Credentials.findOne({ email });
 
-                    if (res.ok) {
-                        return user;
+                    if (!ExistingCredentials) {
+                        const res = await fetch("https://collections-lemon.vercel.app/api/user", {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify({
+                                name, email
+                            })
+                        });
+
+                        if (res.ok) {
+                            return user;
+                        }
                     }
 
                 } catch (error) {
@@ -35,7 +42,7 @@ const authOptions = {
                 }
 
             }
-            
+
             return user;
         }
     }
